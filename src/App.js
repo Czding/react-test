74ffcd813eaee9875f5be4d96208d7e7c777d43b
dingshengfei <275307605@qq.com>
2019-11-18 15:43:30 +0800

    分支修改name3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <form>
-        <label for="name">Name2</label>
+        <label for="name">Name3</label>
         <input type="text" id="name" name="name" required></input>
         <label for="description">Short description</label>
         <input type="text" id="description" name="description" required></input>
